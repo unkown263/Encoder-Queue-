9335af27ab4cbdbfeec54a65f7855c4ebf50258c
Mradul <nirusakimalaal@gmail.com>
2022-05-06 13:16:36 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -174,15 +174,14 @@ async def something():
                     ok = await upload_file(
                         client=e.client,
                         file=f,
-                        file_name=og,
                         caption=og,
-                        name=out,
+                        name=og,
                         progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
                             progress(d, t, nnn, ttt, "uploading..")
                         ),
                     )
                 ds = await e.client.send_file(
-                    e.chat_id, file=ok, file_name=og, caption=og, force_document=True, thumb=thum
+                    e.chat_id, file=ok, file_name=og, caption=og, name=og, force_document=True, thumb=thum
                 )
                 await nnn.delete()
                 org = int(Path(dl).stat().st_size)
